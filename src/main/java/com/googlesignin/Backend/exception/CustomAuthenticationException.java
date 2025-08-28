5d107e68a65800490476d9f0cdb9a0fb41377c65
Joshua Iluma <Joshuailuma@gmail.com>
2025-08-28 09:12:03 +0100

    Remove unused imports

--- src/main/java/com/googlesignin/Backend/exception/CustomAuthenticationException.java
+++ src/main/java/com/googlesignin/Backend/exception/CustomAuthenticationException.java
@@ -1,7 +1,6 @@
 package com.googlesignin.Backend.exception;
 
-import jakarta.persistence.GeneratedValue;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
